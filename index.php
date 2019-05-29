5962d3a8343e72e75ac29164f2441723d753d6db
Jakub <jakub.soad@gmail.com>
2019-05-29 09:58:31 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mroz
+ * Date: 2019-05-29
+ * Time: 09:47
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+
+    </title>
+</head>
+<body>
+
+</body>
+
+</html>
+
